46ddd45a3d07c256b1845bbc6afc18920fd7c6b8
jayden-lee <gwanggi.lee@gmail.com>
2021-02-14 00:08:03 +0900

    Add playground example
    struct

--- 99. Playground/struct.go
+++ 99. Playground/struct.go
@@ -2,6 +2,7 @@ package main
 
 import "fmt"
 
+// type 키워드, 사용자 정의 타입 이름, 기본 타입
 type user struct {
 	id   string
 	name string
@@ -14,8 +15,14 @@ func main() {
 	fmt.Println(user1)
 
 	user2 := user{"id", "name"}
+	changeUserName(&user2)
 	fmt.Println(user2)
 
 	user3 := new(user)
 	fmt.Println(user3)
+}
+
+func changeUserName(u *user) {
+	u.id = "new id"
+	u.name = "new name"
 }
